e8df7f6c91ce0d2b1872109e6d99fcf45793fff3
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-08-27 15:24:40 +0300

    Add project files.

---
+++ HttpRequests/HttpBruteForce.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequests
+{
+    public class HttpBruteForce
+    {
+        private readonly int _parralelCount;
+        private readonly Stack<string> _urls;
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public HttpBruteForce(Stack<string> urls, string pathResult, int parralelCount = 10)
+        {
+            _parralelCount = parralelCount;
+            _urls = urls;
+        }
+
+
+        public void StartBruteForce()
+        {
+            List<Task> tasks = new List<Task>();
+
+            while (_urls.Count > 0)
+            {
+                tasks.Add(brutForceAsync(_urls.Pop()));
+
+                if (tasks.Count > _parralelCount)
+                {
+                    Task.WaitAny(tasks.ToArray());
+                    tasks = CleanFinishTasks(tasks);
+                }
+            }
+            if (tasks.Count > 0)
+            {
+                Task.WaitAny(tasks.ToArray());
+                CleanFinishTasks(tasks);
+            }
+                
+        }
+
+        private List<Task> CleanFinishTasks(List<Task> tasks)
+        {
+            List<Task> cleanTaskList = new List<Task>();
+
+            foreach (var task in tasks)
+            {
+                if (!task.IsCompleted)
+                {
+                    cleanTaskList.Add(task);
+                }
+            }
+
+            return cleanTaskList;
+        }
+
+        private async Task brutForceAsync(string webUrl)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(webUrl);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                logger.Info($"ok {webUrl}");
+            }
+            catch (HttpRequestException e)
+            {
+                logger.Error(e, webUrl);
+            }
+        }
+    }
+}
---
+++ HttpRequests/Program.cs
@@ -0,0 +1,61 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ConfigureLogger();
+
+            var urls = new Stack<string>();
+
+            urls.Push("https:\\\\ya.ru");
+            urls.Push("https:\\\\google.com");
+            urls.Push("https:\\\\ya.ru");
+            urls.Push("https:\\\\google.com");
+            urls.Push("https:\\\\ya.ru");
+            urls.Push("https:\\\\google.com");
+            urls.Push("https:\\\\google.com");
+            urls.Push("https:\\\\google.com");
+            urls.Push("https:\\\\google.com");
+            urls.Push("https:\\\\google.ru");
+            urls.Push("https:\\\\g132343.ru");
+
+            HttpBruteForce httpBruteForce = new HttpBruteForce(urls, "", 2);
+
+            httpBruteForce.StartBruteForce();
+
+        }
+
+
+        static void ConfigureLogger()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile")
+            {
+                FileName = "file.txt",
+                Layout = "${longdate}|${level:uppercase=true}|${logger}|${message}"
+            };
+            //var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            // config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Info, LogLevel.Error, logfile);
+
+
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+        }
+    }
+}
